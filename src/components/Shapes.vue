a121a3035e828c2b9ef457aad3a62549534a260c
cnotv <contact@cnotv.xyz>
2017-11-03 21:03:57 +0100

    added second game

--- src/components/Shapes.vue
+++ src/components/Shapes.vue
@@ -1,7 +1,5 @@
 <template>
-  <div class="container">
-    <h1>Shapes</h1>
-  </div>
+  <div v-on:click="activate"></div>
 </template>
 
 <script>
@@ -9,15 +7,77 @@ export default {
   name: 'Shapes',
   data () {
     return {
+      finish: 10,
+      current: 1,
+      voices: window.speechSynthesis.getVoices()
     }
+  },
+  methods: {
+    hue () {
+      return 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')'
+    },
+
+    size (max = 200, min = 50, unit = 'px') {
+      return Math.floor(Math.random() * (max - min)) + min + unit
+    },
+
+    speak (text) {
+      const msg = new SpeechSynthesisUtterance()
+      msg.text = text
+      msg.voice = this.voices[10]
+      window.speechSynthesis.speak(msg)
+    },
+
+    randomize (el) {
+      Object.assign(
+        el.style, {
+          background: this.hue(),
+          height: this.size(200, 50, 'px'),
+          width: this.size(200, 50, 'px'),
+          marginTop: this.size(-25, 25, 'vh'),
+          marginLeft: this.size(-25, 25, 'vw')
+        }
+      )
+      el.innerText = this.current++
+
+      if (el.innerText === this.finish) {
+        this.current = 1
+      }
+    },
+
+    activate (event) {
+      this.randomize(this.$el)
+      window.speechSynthesis.cancel()
+      this.speak(this.$el.innerText)
+    }
+  },
+
+  mounted () {
+    this.randomize(this.$el)
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style>
-.container {
-  padding: 3rem;
-  line-height: 3;
-}
+<style scoped>
+  html {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    width: 100vw;
+  }
+  div {
+    display: flex;
+    background: red;
+    color: white;
+    font-size: 2rem;
+    align-items: center;
+    justify-content: center;
+    font-weight: 700;
+    transition: all .2s;
+    user-select: none;
+    cursor: pointer;
+    border-radius: .5vw;
+  }
 </style>
